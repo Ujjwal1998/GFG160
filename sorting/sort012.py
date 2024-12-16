94f55d6e13c4173c33a8e63e7142b22c92096edf
Ujjwal1998 <mail2ujjwal98@gmail.com>
2024-12-16 11:31:45 +0530

    21/160

---
+++ sorting/sort012.py
@@ -0,0 +1,18 @@
+def sort012(arr):
+    # code here
+    low, med, high = 0, 0, len(arr) - 1
+    while med <= high:
+        if arr[med] == 0:
+            arr[low], arr[med] = arr[med], arr[low]
+            low += 1
+            med += 1
+        elif arr[med] == 1:
+            med += 1
+        else:
+            arr[med], arr[high] = arr[high], arr[med]
+            high -= 1
+        print(arr, arr[med], arr[low])
+    return arr
+
+
+print(sort012([0, 1, 2, 0, 1, 2]))
